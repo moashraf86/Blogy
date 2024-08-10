4831e4ea9dde6c03b77af5af9a8c3942ca6133bb
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-10 06:28:53 +0300

    refactor: Update CommentList component to use custom hook for fetching comments with useQuery

--- src/components/layout/CommentList.jsx
+++ src/components/layout/CommentList.jsx
@@ -1,65 +1,22 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState, useContext } from "react";
-import {
-  collection,
-  getDocs,
-  query,
-  orderBy,
-  updateDoc,
-  doc,
-} from "firebase/firestore";
-import { db } from "../../utils/firebase";
 import { Skeleton } from "../ui/skeleton";
 import { Comment } from "./Comment";
 import { Alert, AlertDescription } from "../ui/alert";
-import { CommentsContext } from "../../context/CommentsContext";
 import { RiErrorWarningLine } from "@remixicon/react";
+import { useFetchComments } from "../../hooks/useFetchComments";
 
 export const CommentList = ({ post, commentToEdit, handleDelete }) => {
-  const { comments, CommentsDispatch } = useContext(CommentsContext);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
   /**
-   * fetch comments from firebase
+   * Custom Hook to fetch comments
    */
-  const fetchComments = async () => {
-    try {
-      setLoading(true);
-      setError(null);
-      const commentsQuery = query(
-        collection(db, "posts", post?.id, "comments"),
-        orderBy("createdAt", "desc")
-      );
-      const commentsSnapshot = await getDocs(commentsQuery);
-      const commentsList = commentsSnapshot.docs.map((doc) => doc.data());
-      CommentsDispatch({ type: "FETCH_COMMENTS", payload: commentsList });
-      setLoading(false);
-      // update commentCount in the post
-      await updateDoc(doc(db, "posts", post?.id), {
-        commentsCount: commentsList.length,
-      });
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchComments();
-    // clear comments on unmount
-    return () => {
-      CommentsDispatch({ type: "RESET_COMMENTS" });
-    };
-  }, []);
+  const { data: comments, isLoading, isError, error } = useFetchComments(post);
 
   return (
     <div id="comments">
       <h3 className="text-primary font-bold text-xl mb-6">
         Comments ({comments?.length})
       </h3>
-      {loading && (
+      {isLoading && (
         <div className="flex flex-col gap-4 rounded-md bg-primary/10 p-4">
           <div className="flex items-center gap-2">
             <Skeleton className="w-8 h-8 rounded-full" />
@@ -71,13 +28,13 @@ export const CommentList = ({ post, commentToEdit, handleDelete }) => {
           </div>
         </div>
       )}
-      {error && (
+      {isError && (
         <Alert variant="danger" className="flex items-center gap-3">
           <RiErrorWarningLine size={20} className="fill-danger" />
           <AlertDescription>{error}</AlertDescription>
         </Alert>
       )}
-      {!loading && !error && comments?.length === 0 && (
+      {!isLoading && !isError && comments?.length === 0 && (
         <p>No comments added yet</p>
       )}
       {comments &&
