2d69728e194bfc23f55e181013953933f57b4bb9
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-13 06:52:20 +0300

    refactor: Update ModeToggle component to toggle between light and dark themes

--- src/components/shared/ModeToggler.jsx
+++ src/components/shared/ModeToggler.jsx
@@ -1,42 +1,36 @@
 import { Button } from "../ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { useTheme } from "../../context/ThemeProviderContext";
 import { RiMoonLine, RiSunLine } from "@remixicon/react";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
+  /**
+   * Toggle Theme
+   */
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else if (theme === "dark") {
+      setTheme("light");
+    }
+  };
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <RiSunLine
-            size={18}
-            className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-          />
-          <RiMoonLine
-            size={18}
-            className="absolute rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100"
-          />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="rounded-full"
+      onClick={toggleTheme}
+    >
+      <RiSunLine
+        size={18}
+        className="rotate-90 scale-0 transition-transform duration-300 dark:-rotate-0 dark:scale-100"
+      />
+      <RiMoonLine
+        size={18}
+        className="absolute rotate-0 scale-100 transition-transform duration-300 dark:-rotate-90 dark:scale-0"
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
