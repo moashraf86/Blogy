c0f2df6b1d0d5a3d967d19c1de2ac0b441184d3f
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-13 06:54:40 +0300

    refactor: Update alert variant styles and add warning variant

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -4,17 +4,19 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
+  "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-3 [&>svg]:text-foreground [&>svg~*]:pl-8",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        info: "bg-background text-foreground",
+        warning:
+          "border-warning/50 text-warning dark:border-warning [&>svg]:text-warning",
         danger:
           "border-danger/50 text-danger dark:border-danger [&>svg]:text-danger",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "info",
     },
   }
 );
