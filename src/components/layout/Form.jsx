36818317fe9a2c19ecc82107dd3fff846671df13
moashraf86 <botoashraf123@gmail.com>
2024-10-04 04:02:04 +0300

    Refactor Form Component:
    - Added description field
    - Replaced `input` with `textarea` for title and description fields
    - Implemented `autoResize` function for title and description fields
    - Integrated `charCountDisplay` component and added `handleCharCount` logic
    - Adjusted Publish `Button` to be fixed at the bottom on mobile devices
    - Disabled Publish `Button` when `notReadyForSubmit` is true

--- src/components/layout/Form.jsx
+++ src/components/layout/Form.jsx
@@ -15,8 +15,10 @@ import { tags } from "../../utils/tags";
 import { Button } from "../ui/button";
 import { SignInModal } from "../shared/SignInModal";
 import YooptaTextEditor from "./YooptaEditor";
+import { CharCountDisplay } from "../shared/CharCountDisplay";
 export const Form = ({
   title,
+  description,
   content,
   image,
   tag,
@@ -33,6 +35,7 @@ export const Form = ({
   const { currentUser } = useContext(AuthContext);
   const isGuest = currentUser?.isGuest;
   const [showModal, setShowModal] = useState(false);
+  const [charsCount, setCharsCount] = useState(0);
   /**
    * Hide the alert message
    */
@@ -42,10 +45,46 @@ export const Form = ({
   // destructuring the errors object
   const {
     title: titleError,
+    description: descriptionError,
     content: contentError,
     tag: tagError,
     image: imageError,
   } = errors;
+
+  /**
+   * Check if the form is ready to be submitted
+   */
+  const notReadyForSubmit =
+    !title ||
+    !description ||
+    !content ||
+    !tag ||
+    (isImageRequired && !image) ||
+    titleError.hasError ||
+    descriptionError.hasError ||
+    contentError.hasError ||
+    tagError.hasError ||
+    imageError.hasError;
+
+  /**
+   * Auto resize the textarea
+   */
+
+  const autoResize = (e) => {
+    e.target.style.height = "auto";
+    e.target.style.height = e.target.scrollHeight + "px";
+  };
+
+  /**
+   * Set the character count
+   * @param {string} value - counted characters comes from calcContentChars function
+   * @returns {void}
+   * sets the character count state to the length of the content
+   */
+  const handleCharCount = (value) => {
+    setCharsCount(value);
+  };
+
   return (
     <>
       {/* If user is a guest, show a message to log in with Google to publish posts */}
@@ -83,7 +122,7 @@ export const Form = ({
               onSubmit={onsubmit}
               className="flex flex-col gap-4 items-start"
             >
-              <div className="flex flex-col items-start sm:flex-row sm:items-center gap-3">
+              <div className="flex flex-col items-start sm:flex-row sm:items-center gap-3 w-full">
                 <div className="flex flex-col gap-1">
                   <ComboboxDemo
                     onSelect={onSelect}
@@ -159,26 +198,61 @@ export const Form = ({
                     )}
                   </div>
                 </div>
+                {/* Publish Button */}
+                <div className="fixed bottom-0 left-0 right-0 bg-background z-20 py-3 px-6 border-t border-border shadow-sm flex flex-1 justify-end md:static md:p-0 md:border-none md:shadow-none md:bg-none">
+                  <Button
+                    disabled={notReadyForSubmit}
+                    type="submit"
+                    className="disabled:opacity-20"
+                    size="lg"
+                  >
+                    {isGuest ? "Save to Drafts" : "Publish"}
+                  </Button>
+                </div>
               </div>
               {/* Post Title */}
               <div className="flex flex-col gap-1 self-stretch">
-                <input
+                <textarea
                   name="title"
                   id="title"
-                  className={`w-full p-2 text-primary bg-transparent rounded-md text-2xl md:text-4xl  md:leading-[1.5] font-bold focus-visible:outline-none ${
-                    !titleError.hasError && "border-input"
-                  } ${titleError.hasError && "border-danger"}`}
+                  className="w-full p-2 text-primary bg-transparent rounded-md text-2xl md:text-4xl font-bold focus-visible:outline-none resize-none overflow-hidden"
                   value={title}
                   type="text"
+                  rows={1}
                   placeholder="Add title"
+                  onInput={(e) => autoResize(e)}
                   onChange={(e) =>
                     handleChange({
                       target: { name: "title", value: e.target.value },
                     })
                   }
-                />
+                ></textarea>
                 {titleError.hasError && (
                   <p className="text-sm text-danger">{titleError.message}</p>
+                )}
+              </div>
+              {/* Post Description */}
+              <div className="flex flex-col gap-1 self-stretch">
+                <textarea
+                  name="description"
+                  id="description"
+                  autoComplete="off"
+                  className="w-full p-2 text-primary bg-transparent rounded-md text-lg md:text-xl  md:leading-[1.5] focus-visible:outline-none resize-none overflow-hidden"
+                  value={description}
+                  type="text"
+                  rows={1}
+                  placeholder="Add description"
+                  onInput={(e) => autoResize(e)}
+                  onChange={(e) =>
+                    handleChange({
+                      target: { name: "description", value: e.target.value },
+                    })
+                  }
+                ></textarea>
+                {descriptionError.hasError && (
+                  <p className="text-sm text-danger">
+                    {descriptionError.message}
+                  </p>
                 )}
               </div>
               {/* Display the image */}
@@ -221,22 +295,24 @@ export const Form = ({
                   />
                 </div>
               )}
-              <div className="flex flex-col gap-1 self-stretch">
+              <div className="flex flex-col gap-1 self-stretch pb-6 md:pb-0">
                 <YooptaTextEditor
-                  onChange={(content) =>
+                  handleCharCount={handleCharCount}
+                  onChange={(value) =>
                     handleChange({
-                      target: { name: "content", value: content },
+                      target: {
+                        name: "content",
+                        value: value,
+                      },
                     })
                   }
-                  value={content}
+                  defaultValue={content}
                 />
                 {contentError.hasError && (
                   <p className="text-sm text-danger">{contentError.message}</p>
                 )}
+                <CharCountDisplay charCount={charsCount} maxChars={10000} />
               </div>
-              <Button type="submit" className="self-end mt-3" size="lg">
-                {isGuest ? "Save to Drafts" : "Publish"}
-              </Button>
             </form>
           </div>
         </div>
